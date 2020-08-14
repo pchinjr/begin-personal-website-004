6a256d0d01854143d3143592d40a92db7486456b
Paul Chin Jr <pchinjr@gmail.com>
2020-08-13 21:11:42 -0400

    Update index.js

--- src/http/get-index/index.js
+++ src/http/get-index/index.js
@@ -39,6 +39,7 @@ exports.handler = async function Index () {
       'content-type': 'text/html; charset=utf8',
       'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
     },
+    statusCode: 200,
     body
   }
 }
